6b8b42405fbe72e148e03355a888fc45f7a84284
leonard <leogurning@gmail.com>
2023-12-30 22:01:20 +0700

    fix packages json and add vercel json for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import bodyParser from 'body-parser';
 import foodRouter from './routers/food.router';
 import userRouter from './routers/user.router';
 
@@ -18,8 +19,21 @@ app.use(express.json());
 
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin:["http://localhost:4200", /\.vercel\.app$/],
+    maxAge: 86400
 }));
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Used by load balancer to make sure we are taking requests
+app.get('/healthcheck', (req, res) => res.send('System status: Ok'));
+
+// basic routes
+app.get('/', (req, res) => {
+  res.send(`tokoFood API is running at PORT:${port}/api`);
+});
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
@@ -27,5 +41,5 @@ app.use("/api/orders", orderRouter);
 
 const port = 5000;
 app.listen(port, () => {
-    console.log("Website served on http://localhost:" + port);
+    console.log("Website API Server is listening at PORT " + port);
 });
